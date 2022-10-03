a34f5eb59a78415975dd62380d7e078d91e59b2c
mitrandir-pl <leshkopavel@gmail.com>
2022-10-03 08:20:50 +0300

    fix block date service

--- innotter_django/users/services.py
+++ innotter_django/users/services.py
@@ -1,9 +1,7 @@
 from datetime import datetime
-from rest_framework import exceptions
 from pages.serializers import PageSerializer, PageSimpleSerializer
 from pages.models import Page
 from users.models import User
-from core.error_messages import WRONG_DATE_MESSAGE
 
 
 class ModeratorService:
@@ -13,7 +11,9 @@ class ModeratorService:
         Check if date is valid with the serializer, and if it's okay,
         returns serialized page.
         """
-        unblock_date = self.get_date(data)
+        unblock_date_str = data.get('unblock_date')
+        unblock_date = datetime.strptime(unblock_date_str,
+                                         '%Y-%m-%d %H:%M:%S')
         page = Page.objects.get(pk=user_pk)
         page.unblock_date = unblock_date
         data = PageSimpleSerializer(page).data
@@ -21,24 +21,6 @@ class ModeratorService:
         if serialized_page.is_valid(raise_exception=True):
             serialized_page.save()
             return serialized_page
-
-    def get_date(self, data: dict[str: str]) -> datetime:
-        """
-        Method takes a dict, get values
-        (year, month, day and etc.), from it
-        and returns it in datetime format.
-        """
-        year = data.get('year', 0)
-        month = data.get('day', 0)
-        day = data.get('day', 0)
-        hour = data.get('hour', 0)
-        minute = data.get('minute', 0)
-        try:
-            unblock_date_list = [year, month, day, hour, minute]
-            unblock_date = datetime(*[int(i) for i in unblock_date_list])
-            return unblock_date
-        except TypeError:
-            raise exceptions.ValidationError(WRONG_DATE_MESSAGE)
 
 
 class AdminService(ModeratorService):
