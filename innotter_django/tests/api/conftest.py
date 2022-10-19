dc052e823a7754537196bdbc4ac91fefe8cae57a
mitrandir-pl <leshkopavel@gmail.com>
2022-10-19 10:59:24 +0300

    celery+aws fixes

--- innotter_django/tests/api/conftest.py
+++ innotter_django/tests/api/conftest.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from faker import Faker
 from pytest_factoryboy import register
 from django.urls import reverse
+from rest_framework.response import Response
 from rest_framework.test import APIClient
 from tests.api.factories import PageFactory, UserFactory, PostFactory
 
@@ -15,7 +16,7 @@ register(PostFactory)
 
 
 @pytest.fixture
-def user_password():
+def user_password() -> str:
     return 'password'
 
 
@@ -25,7 +26,7 @@ def client():
 
 
 @pytest.fixture
-def user_for_serializer():
+def user_for_serializer() -> dict[str: str]:
     faker = Faker()
     return {
         'username': faker.first_name(),
@@ -37,24 +38,24 @@ def user_for_serializer():
 
 
 @pytest.fixture
-def refresh_token(client, user, user_password):
+def refresh_token(client, user: UserFactory, user_password: str) -> str:
     response = login(client, user.email, user_password)
     return response.json()['refresh_token']
 
 
 @pytest.fixture
-def admin_access_token(client, admin, user_password):
+def admin_access_token(client, admin: UserFactory, user_password: str) -> str:
     response = login(client, admin.email, user_password)
     return response.json()['access_token']
 
 
 @pytest.fixture
-def access_token(client, user, user_password):
+def access_token(client, user: UserFactory, user_password: str) -> str:
     response = login(client, user.email, user_password)
     return response.json()['access_token']
 
 
-def login(client, email, password):
+def login(client, email: str, password: str) -> Response:
     url = reverse('users-login')
     data = {'user': {
         'email': email,
@@ -64,7 +65,7 @@ def login(client, email, password):
 
 
 @pytest.fixture
-def page_for_serializer(user):
+def page_for_serializer(user: UserFactory) -> dict[str: str]:
     faker = Faker()
     return {
         'name': faker.name(),
@@ -77,7 +78,7 @@ def page_for_serializer(user):
 
 
 @pytest.fixture
-def post_for_serializer(user, not_private_page):
+def post_for_serializer(user: UserFactory, not_private_page: PageFactory) -> dict[str: str]:
     faker = Faker()
     return {
         'page': not_private_page.id,
@@ -88,7 +89,7 @@ def post_for_serializer(user, not_private_page):
 
 
 @pytest.fixture
-def unblock_date():
+def unblock_date() -> dict[str: str]:
     unblock_date = datetime.now() + timedelta(days=1)
     unblock_date = str(unblock_date.replace(microsecond=0))
     return {'unblock_date': unblock_date}
--- innotter_django/tests/api/test_post_views.py
+++ innotter_django/tests/api/test_post_views.py
@@ -13,33 +13,33 @@ class TestPostAPI:
         assert response.status_code == status.HTTP_201_CREATED
 
     def test_set_like(self, client, user, post, access_token):
-        client.post(reverse('posts-set-like', kwargs={'pk': 1}),
+        client.post(reverse('posts-set-like', kwargs={'pk': post.id}),
                     HTTP_AUTHORIZATION=f'token {access_token}')
         assert post.likes.first() == user
-        client.post(reverse('posts-set-like', kwargs={'pk': 1}),
+        client.post(reverse('posts-set-like', kwargs={'pk': post.id}),
                     HTTP_AUTHORIZATION=f'token {access_token}')
         assert post.likes.first() is None
 
     def test_like_user_list(self, client, post, user, access_token):
-        client.post(reverse('posts-set-like', kwargs={'pk': 1}),
+        client.post(reverse('posts-set-like', kwargs={'pk': post.id}),
                     HTTP_AUTHORIZATION=f'token {access_token}')
-        response = client.get(reverse('posts-like-user-list', kwargs={'pk': 1}),
+        response = client.get(reverse('posts-like-user-list', kwargs={'pk': post.id}),
                               HTTP_AUTHORIZATION=f'token {access_token}')
         assert response.status_code == status.HTTP_200_OK
         assert type(response.json()) is list
         assert response.json()[0].get('id') == user.pk
 
     def test_total_likes(self, client, post, user, access_token):
-        url = reverse('posts-total-likes', kwargs={'pk': 1})
+        url = reverse('posts-total-likes', kwargs={'pk': post.id})
         response = client.get(url, HTTP_AUTHORIZATION=f'token {access_token}')
         assert response.data.get('total_likes') == 0
-        client.post(reverse('posts-set-like', kwargs={'pk': 1}),
+        client.post(reverse('posts-set-like', kwargs={'pk': post.id}),
                     HTTP_AUTHORIZATION=f'token {access_token}')
         response = client.get(url, HTTP_AUTHORIZATION=f'token {access_token}')
         assert response.data.get('total_likes') == 1
 
     def test_liked_posts(self, client, post, access_token):
-        client.post(reverse('posts-set-like', kwargs={'pk': 1}),
+        client.post(reverse('posts-set-like', kwargs={'pk': post.id}),
                     HTTP_AUTHORIZATION=f'token {access_token}')
         url = reverse('posts-liked-posts')
         response = client.get(url, HTTP_AUTHORIZATION=f'token {access_token}')
